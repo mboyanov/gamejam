b23b4fcee7e5eca576a5501ff5167350d7f72a38
Martin <mboyanov@gmail.com>
2015-01-24 13:41:01 +0200

    Initial commit

---
+++ Assets/Marco Polo/GameLogic.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : Photon.MonoBehaviour {
+
+	public static int playerWhoIsIt;
+	public static int player1=-1;
+	public static int player2=-1;
+
+	private static PhotonView ScenePhotonView;
+	
+	void Start()
+	{
+		ScenePhotonView = this.GetComponent<PhotonView>();
+	}
+	
+	void OnPhotonPlayerConnected(PhotonPlayer player)
+	{
+
+		Debug.Log("OnPhotonPlayerConnected: " + player);
+		Debug.Log ("player 1" + player1);
+		Debug.Log ("player 2" + player2);
+
+		// when new players join, we send "who's it" to let them know
+		// only one player will do this: the "master"
+		
+		if (PhotonNetwork.isMasterClient)
+		{
+			TagPlayer(playerWhoIsIt);
+		}
+	}
+	
+	public static void TagPlayer(int playerID)
+	{
+		Debug.Log("TagPlayer: " + playerID);
+		ScenePhotonView.RPC("TaggedPlayer", PhotonTargets.All, playerID);
+	}
+
+	void OnPhotonPlayerDisconnected(PhotonPlayer player)
+	{
+		Debug.Log("OnPhotonPlayerDisconnected: " + player);
+		
+		if (PhotonNetwork.isMasterClient)
+		{
+			if (player.ID == playerWhoIsIt)
+			{
+				// if the player who left was "it", the "master" is the new "it"
+				TagPlayer(PhotonNetwork.player.ID);
+			}
+		}
+	}
+
+	void OnJoinedRoom()
+	{
+		// game logic: if this is the only player, we're "it"
+		if (PhotonNetwork.playerList.Length == 1)
+		{
+			playerWhoIsIt = PhotonNetwork.player.ID;
+		}
+		
+		Debug.Log("onjoinedroom playerWhoIsIt: " + playerWhoIsIt);
+	}
+
+
+	[RPC]
+	void TaggedPlayer(int playerID)
+	{
+		playerWhoIsIt = playerID;
+		Debug.Log("RPC TaggedPlayer: " + playerID);
+	}
+}
---
+++ Assets/Marco Polo/RandomMatchmaker.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class RandomMatchmaker : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room!");
+		PhotonNetwork.CreateRoom("main");
+
+	}
+
+	void OnJoinedRoom()
+	{
+		GameObject monster;
+		// game logic: if this is the only player, we're "it"
+		if (PhotonNetwork.playerList.Length == 1) {
+					 GameLogic.player1 = PhotonNetwork.player.ID;
+					 monster = PhotonNetwork.Instantiate ("monsterprefab", Vector3.zero, Quaternion.identity, 0);
+			Camera.current.cullingMask ^= 1<<10;
+				} else {
+					GameLogic.player2 = PhotonNetwork.player.ID;
+					monster = PhotonNetwork.Instantiate("monsterprefab2", Vector3.zero, Quaternion.identity, 0);
+			Camera.current.cullingMask ^= 1<<9;
+				}
+
+
+		monster.GetComponent<myThirdPersonController>().isControllable = true;
+	}
+
+
+
+
+}
